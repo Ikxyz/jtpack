6aa7ad815ef4aa1830f3e30a0b7d404a2a093f6a
Ik Ben <40415727+Ikxyz@users.noreply.github.com>
2023-02-26 02:16:36 +0100

    fix error

--- lib/jtpack.dart
+++ lib/jtpack.dart
@@ -1,8 +1,5 @@
 library jtpack;
 
-import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:jtpack/.config.dart';
 
 export "./modules/_modules.dart";

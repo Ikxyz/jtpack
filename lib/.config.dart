3dbccaf642b23eea5e7a0116491e2b79d673cd4a
Ik Ben <40415727+Ikxyz@users.noreply.github.com>
2023-02-26 02:03:45 +0100

    bit refacftor

--- lib/.config.dart
+++ lib/.config.dart
@@ -1,3 +1,1 @@
 import 'package:flutter/material.dart';
-
-GlobalKey<NavigatorState>  navigatorGlobalKey =  GlobalKey<NavigatorState>();
--- lib/jtpack.dart
+++ lib/jtpack.dart
@@ -1,3 +1,11 @@
 library jtpack;
 
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
 export "./modules/_modules.dart";
+
+class JtPack {
+  GlobalKey<NavigatorState> navigatorGlobalKey = GlobalKey<NavigatorState>();
+}
